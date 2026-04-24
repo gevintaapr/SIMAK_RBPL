17e6764101cac6e5820df4d7a40acd15ba02e04b
gevintaapr <gevintap@gmail.com>
2026-04-24 16:05:28 +0700

    Fix PDO database connection for Pimpinan

--- config/database.php
+++ config/database.php
@@ -1,15 +1,24 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "simakhcts";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
     public $conn;
+
+    public function __construct() {
+        $this->host = getenv('MYSQLHOST') ?: "localhost";
+        $this->db_name = getenv('MYSQLDATABASE') ?: "simakhcts";
+        $this->username = getenv('MYSQLUSER') ?: "root";
+        $this->password = getenv('MYSQLPASSWORD') ?: "";
+        $this->port = getenv('MYSQLPORT') ?: "3306";
+    }
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $exception) {
